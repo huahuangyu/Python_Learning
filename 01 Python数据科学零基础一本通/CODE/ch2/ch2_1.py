07017d55f5f2b1b500c3ed91e8e77a20949b933e
huahuangyu <huahuangy@gmail.com>
2021-09-04 21:15:30 +0800

    update

---
+++ 01 Python数据科学零基础一本通/CODE/ch2/ch2_1.py
@@ -0,0 +1,9 @@
+# 使用程序计算每年可以存储多少钱
+# ch2_1.py
+
+hourly_salary = 125
+annual_salary = hourly_salary * 8 * 300
+monthly_fee = 9000
+annual_fee = monthly_fee * 12
+annual_savings = annual_salary - annual_fee
+print(annual_savings)
